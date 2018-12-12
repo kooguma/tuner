5e1656905c4eded6b5157edccf96c819e98f6c84
lzz <liuzizhang1994@gmail.com>
2018-12-12 22:35:23 +0800

    fix some ui

--- app/src/main/java/top/koguma/tuner/activity/HelpActivity.java
+++ app/src/main/java/top/koguma/tuner/activity/HelpActivity.java
@@ -1,7 +1,9 @@
 package top.koguma.tuner.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
 import com.laputapp.ui.BaseActivity;
 import top.koguma.tuner.R;
@@ -10,14 +12,25 @@ public class HelpActivity extends BaseActivity {
 
     TextView txtCommon;
     TextView txtOther;
+    TextView txtSend;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         txtCommon = (TextView) findViewById(R.id.txt_common);
         txtOther = (TextView) findViewById(R.id.txt_other);
+        txtSend = (TextView) findViewById(R.id.txt_send);
         txtCommon.setText(Html.fromHtml(getString(R.string.help_common_problems_content)));
         txtOther.setText(Html.fromHtml(getString(R.string.help_other_problems_content)));
-
+        txtSend.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("message/rfc822");
+                email.putExtra(Intent.EXTRA_EMAIL, new String[] { "ToonikaTuner.com" });
+                email.putExtra(Intent.EXTRA_SUBJECT, "");
+                email.putExtra(Intent.EXTRA_TEXT, "");
+                startActivity(Intent.createChooser(email, "Toonika Tuner"));
+            }
+        });
     }
 }
--- app/src/main/java/top/koguma/tuner/activity/ToningActivity.java
+++ app/src/main/java/top/koguma/tuner/activity/ToningActivity.java
@@ -2,6 +2,7 @@ package top.koguma.tuner.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import top.koguma.tuner.Navigator;
 import top.koguma.tuner.R;
@@ -13,12 +14,15 @@ public class ToningActivity extends TunerBaseActivity implements View.OnClickLis
     TextView btnHelp;
 
     TextView[] btnStrings = new TextView[4];
+    ImageView imageView;
 
     TitleSwitchTab tabSwitch;
     ToningIndicator indicator;
 
     int[] titles = new int[] { R.string.toning_violin, R.string.toning_viola,
         R.string.toning_cello };
+
+    int currentTitle = titles[0];
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,60 +37,75 @@ public class ToningActivity extends TunerBaseActivity implements View.OnClickLis
         tabSwitch.setTabs(titles);
         tabSwitch.setOnTabChangeListener(new TitleSwitchTab.OnTabChangeListener() {
             @Override public void onTabChange(int curPos) {
-
+                updateBtnStringsName(curPos);
             }
         });
 
-        btnStrings[0] = (TextView) findViewById(R.id.btn_string_g);
-        btnStrings[1] = (TextView) findViewById(R.id.btn_string_d);
-        btnStrings[2] = (TextView) findViewById(R.id.btn_string_a);
-        btnStrings[3] = (TextView) findViewById(R.id.btn_string_e);
+        btnStrings[0] = (TextView) findViewById(R.id.btn_string_1);
+        btnStrings[1] = (TextView) findViewById(R.id.btn_string_2);
+        btnStrings[2] = (TextView) findViewById(R.id.btn_string_3);
+        btnStrings[3] = (TextView) findViewById(R.id.btn_string_4);
 
         for (TextView t : btnStrings) {
             t.setOnClickListener(this);
         }
+
+        imageView = (ImageView) findViewById(R.id.img_instrument);
     }
 
     @Override public void onClick(View v) {
+        updateBtnStringsStatus(v);
         switch (v.getId()) {
             case R.id.btn_help:
                 Navigator.startHelpActivity(this);
                 break;
-            case R.id.btn_string_g:
-                btnStrings[0].setActivated(true);
-                btnStrings[1].setActivated(false);
-                btnStrings[2].setActivated(false);
-                btnStrings[3].setActivated(false);
+            case R.id.btn_string_1:
+
                 break;
-            case R.id.btn_string_d:
-                btnStrings[0].setActivated(false);
-                btnStrings[1].setActivated(true);
-                btnStrings[2].setActivated(false);
-                btnStrings[3].setActivated(false);
+            case R.id.btn_string_2:
+
                 break;
-            case R.id.btn_string_a:
-                btnStrings[0].setActivated(false);
-                btnStrings[1].setActivated(false);
-                btnStrings[2].setActivated(true);
-                btnStrings[3].setActivated(false);
+            case R.id.btn_string_3:
+
                 break;
-            case R.id.btn_string_e:
-                btnStrings[0].setActivated(false);
-                btnStrings[1].setActivated(false);
-                btnStrings[2].setActivated(false);
-                btnStrings[3].setActivated(true);
+            case R.id.btn_string_4:
+
                 break;
             default:
         }
     }
 
-    // public void updateBtnStrings(View v) {
-    //     for (TextView btn : btnStrings) {
-    //         if (btn == v) {
-    //             v.setActivated(true);
-    //         } else {
-    //             v.setActivated(false);
-    //         }
-    //     }
-    // }
+    public void updateBtnStringsStatus(View v) {
+        if (v instanceof TextView) {
+            v.setActivated(true);
+            for (TextView btn : btnStrings) {
+                if (v != btn) btn.setActivated(false);
+            }
+        }
+    }
+
+    public void updateBtnStringsName(int curPos) {
+        switch (curPos) {
+            case 0:
+                btnStrings[0].setText(R.string.toning_string_d);
+                btnStrings[1].setText(R.string.toning_string_g);
+                btnStrings[2].setText(R.string.toning_string_a);
+                btnStrings[3].setText(R.string.toning_string_e);
+                break;
+            case 1:
+                btnStrings[0].setText(R.string.toning_string_c);
+                btnStrings[1].setText(R.string.toning_string_g);
+                btnStrings[2].setText(R.string.toning_string_d);
+                btnStrings[3].setText(R.string.toning_string_a);
+                break;
+            case 2:
+                btnStrings[0].setText(R.string.toning_string_c);
+                btnStrings[1].setText(R.string.toning_string_g);
+                btnStrings[2].setText(R.string.toning_string_d);
+                btnStrings[3].setText(R.string.toning_string_a);
+                break;
+            default:
+                break;
+        }
+    }
 }
